ff089331b88062a58281db52fcf1d7f93184ad05
DiegoNascimento1 <diegosoaresnascimento1@gmail.com>
2022-10-25 14:38:24 -0300

    first commit

---
+++ Mysql/criar_usuario.sql
@@ -0,0 +1,3 @@
+CREATE USER `novousuario`@`%` IDENTIFIED WITH mysql_native_password BY 'Senha';
+GRANT Alter, Alter Routine, Create, Create Routine, Create Temporary Tables, Create View, Delete, Drop, Event, Execute, Index, Insert, Lock Tables, References, Select, Show View, Trigger, Update ON `conta\_digital\_dev`.* TO `novousuario`@`%`;
+GRANT Alter, Alter Routine, Create, Create Routine, Create Temporary Tables, Create View, Delete, Drop, Event, Execute, Index, Insert, Lock Tables, References, Select, Show View, Trigger, Update ON `conta\_digital\_homo`.* TO `novousuario`@`%`;
